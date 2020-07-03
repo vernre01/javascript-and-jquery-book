4c39bbeb4004cf92633fad58b96e8b69de227598
Rebecca Vernon <vernre01@gmail.com>
2020-07-03 16:30:25 -0500

    update first greeting message to say good evening

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -3,7 +3,7 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good afternoon!';
+    greeting = 'Good evening!';
 } else if (hourNow > 12) {
     greeting = 'Good afternoon!';
 } else if (hourNow > 0) {
